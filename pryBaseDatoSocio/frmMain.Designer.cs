438901b9606a0f5f303f601e2c32cffd40b17cf4
Alumnos <Alumnos@PC187>
2023-10-17 14:22:36 -0300

    Agregar archivos de proyecto.

---
+++ pryBaseDatoSocio/frmMain.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace pryBaseDatoSocio
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblEstadoConexion = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblDatos = new System.Windows.Forms.Label();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblEstadoConexion});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 420);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(561, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblEstadoConexion
+            // 
+            this.lblEstadoConexion.Name = "lblEstadoConexion";
+            this.lblEstadoConexion.Size = new System.Drawing.Size(27, 17);
+            this.lblEstadoConexion.Text = "----";
+            // 
+            // lblDatos
+            // 
+            this.lblDatos.AutoSize = true;
+            this.lblDatos.Location = new System.Drawing.Point(13, 13);
+            this.lblDatos.Name = "lblDatos";
+            this.lblDatos.Size = new System.Drawing.Size(35, 13);
+            this.lblDatos.TabIndex = 1;
+            this.lblDatos.Text = "Datos";
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(140, 115);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(240, 150);
+            this.dgv.TabIndex = 2;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 442);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.lblDatos);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "frmMain";
+            this.Text = "EL CLUB ♥";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblEstadoConexion;
+        private System.Windows.Forms.Label lblDatos;
+        private System.Windows.Forms.DataGridView dgv;
+    }
+}
+
---
+++ pryBaseDatoSocio/frmMain.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryBaseDatoSocio
+{
+    public partial class frmMain : Form
+    {
+
+        clsAccesoDatos objBaseDatos;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            objBaseDatos = new clsAccesoDatos();
+
+            objBaseDatos.ConectarBD();
+
+            lblEstadoConexion.Text = objBaseDatos.estadoConexion;
+            
+            lblEstadoConexion.BackColor = Color.Green;
+
+            objBaseDatos.TraerDatos();
+
+            lblDatos.Text = objBaseDatos.datosTabla;
+        }
+    }
+}
